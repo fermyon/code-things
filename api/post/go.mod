6db6bd2d07eddde2399fed22350ea71b538f2cf2
Justin Pflueger <justin.pflueger@fermyon.com>
2023-03-08 12:46:19 -0700

    add json (de)serialization
    Signed-off-by: Justin Pflueger <justin.pflueger@fermyon.com>

--- api/post/go.mod
+++ api/post/go.mod
@@ -4,4 +4,8 @@ go 1.17
 
 require github.com/fermyon/spin/sdk/go v0.9.0
 
-require github.com/go-chi/chi/v5 v5.0.8 // indirect
+require (
+	github.com/go-chi/chi/v5 v5.0.8 // indirect
+	github.com/valyala/fastjson v1.6.4 // indirect
+	golang.org/x/exp v0.0.0-20230307190834-24139beb5833 // indirect
+)
--- api/post/main.go
+++ api/post/main.go
@@ -1,10 +1,17 @@
 package main
 
 import (
+	"context"
+	"fmt"
+	"io"
 	"net/http"
+	"strconv"
+	"strings"
 
 	spinhttp "github.com/fermyon/spin/sdk/go/http"
 	"github.com/go-chi/chi/v5"
+	"github.com/valyala/fastjson"
+	"golang.org/x/exp/slices"
 )
 
 func main() {}
@@ -16,7 +23,7 @@ func init() {
 
 		// mount our routes using the prefix
 		routePrefix := req.Header.Get("Spin-Component-Route")
-		router.Mount(routePrefix, Post())
+		router.Mount(routePrefix, PostRouter())
 
 		// hand the request/response off to chi
 		router.ServeHTTP(res, req)
@@ -25,16 +32,55 @@ func init() {
 
 func PostRouter() chi.Router {
 	posts := chi.NewRouter()
+	idParamPattern := fmt.Sprintf("/{%v:[0-9]+}", postIdCtxKey)
+	posts.Use(PostCtx)
 	posts.Post("/", createPost)
 	posts.Get("/", listPosts)
-	posts.Get("/{id:[0-9]+}", readPost)
-	posts.Put("/{id:[0-9]+}", updatePost)
-	posts.Delete("/{id:[0-9]+}", deletePost)
+	posts.Get(idParamPattern, readPost)
+	posts.Put(idParamPattern, updatePost)
+	posts.Delete(idParamPattern, deletePost)
 	return posts
 }
 
+var postIdCtxKey string = "id"
+
+type postCtxKey struct{}
+
+func PostCtx(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(res http.ResponseWriter, req *http.Request) {
+		var post Post
+		var err error
+
+		if req.ContentLength > 0 && req.Header.Get("Content-Type") == "application/json" {
+			if post, err = ParseJsonPost(req.Body); err != nil {
+				// parsing failed end the request here
+				msg := fmt.Sprintf("Failed to parse the post from request body: %v\n", err)
+				renderBadRequestResponse(res, msg)
+				return
+			}
+			if err = post.Validate(); err != nil {
+				msg := fmt.Sprintf("Request body failed validation: %v\n", err)
+				renderBadRequestResponse(res, msg)
+				return
+			}
+		}
+
+		ctx := context.WithValue(req.Context(), postCtxKey{}, post)
+		next.ServeHTTP(res, req.WithContext(ctx))
+	})
+}
+
+func getPostId(r *http.Request) (int, error) {
+	idStr := chi.URLParam(r, postIdCtxKey)
+	return strconv.Atoi(idStr)
+}
+
 func createPost(res http.ResponseWriter, req *http.Request) {
-	http.Error(res, "Create post not yet implemented", http.StatusNotImplemented)
+	post := req.Context().Value(postCtxKey{}).(Post)
+
+	//TODO: actually write the post to the database
+
+	post.RenderJsonResponse(res)
 }
 
 func listPosts(res http.ResponseWriter, req *http.Request) {
@@ -42,15 +88,41 @@ func listPosts(res http.ResponseWriter, req *http.Request) {
 }
 
 func readPost(res http.ResponseWriter, req *http.Request) {
-	http.Error(res, "Read post not yet implemented", http.StatusNotImplemented)
+	var post Post
+
+	if id, err := getPostId(req); err != nil {
+		msg := fmt.Sprintf("Failed to parse URL param 'id': %v", id)
+		renderBadRequestResponse(res, msg)
+		return
+	} else {
+		// TODO: actually get the post from the database
+		post = Post{
+			ID: id,
+		}
+	}
+
+	post.RenderJsonResponse(res)
 }
 
 func updatePost(res http.ResponseWriter, req *http.Request) {
-	http.Error(res, "Update post not yet implemented", http.StatusNotImplemented)
+	post := req.Context().Value(postCtxKey{}).(Post)
+
+	post.RenderJsonResponse(res)
 }
 
 func deletePost(res http.ResponseWriter, req *http.Request) {
-	http.Error(res, "Delete post not yet implemented", http.StatusNotImplemented)
+	var post Post
+
+	if id, err := getPostId(req); err != nil {
+		msg := fmt.Sprintf("Failed to parse URL param 'id': %v", id)
+		renderBadRequestResponse(res, msg)
+		return
+	} else {
+		// TODO: actually delete the post from the database
+		fmt.Printf("TODO: implement delete for id=%v", id)
+	}
+
+	post.RenderJsonResponse(res)
 }
 
 type Post struct {
@@ -71,4 +143,87 @@ var postTypes = []string{
 // enumerated values for visibility
 var postVisibilities = []string{
 	"public",
+}
+
+func ParseJsonPost(r io.ReadCloser) (Post, error) {
+	var post Post
+
+	// read the request bytes into []byte
+	b, err := io.ReadAll(r)
+	if err != nil {
+		return post, fmt.Errorf("Error reading the request: %v", err)
+	}
+
+	// parse the []byte array
+	var p fastjson.Parser
+	if val, err := p.ParseBytes(b); err != nil {
+		return post, fmt.Errorf("Error parsing json: %v", err)
+	} else {
+		post.ID = val.GetInt("id")
+		post.AuthorID = string(val.GetStringBytes("author_id"))
+		post.Content = string(val.GetStringBytes("content"))
+		post.Type = string(val.GetStringBytes("type"))
+		post.Data = string(val.GetStringBytes("data"))
+		post.Visibility = string(val.GetStringBytes("visibility"))
+		return post, nil
+	}
+}
+
+func (post *Post) Validate() error {
+	var errs []string
+	if post.AuthorID == "" {
+		errs = append(errs, "'author_id' is required")
+	}
+	if post.Content == "" {
+		errs = append(errs, "'content' is required")
+	}
+	if !slices.Contains(postTypes, post.Type) {
+		errs = append(errs, fmt.Sprintf("'type' must be one of [%v]", postTypes))
+	}
+	if post.Data == "" {
+		errs = append(errs, "'data' is required")
+	}
+	if !slices.Contains(postVisibilities, post.Visibility) {
+		errs = append(errs, fmt.Sprintf("'visibility' must be one of [%v]", postTypes))
+	}
+
+	if len(errs) > 0 {
+		return fmt.Errorf("Request failed validation:\n%s", strings.Join(errs, "\n"))
+	} else {
+		return nil
+	}
+}
+
+func (post *Post) ToJson() string {
+	return fmt.Sprintf(`{
+"id": %v,
+"author_id": "%v",
+"content": "%v",
+"type": "%v",
+"data": "%v",
+"visibility": "%v"
+}`,
+		post.ID,
+		post.AuthorID,
+		post.Content,
+		post.Type,
+		post.Data,
+		post.Visibility)
+}
+
+func (post *Post) RenderJsonResponse(res http.ResponseWriter) {
+	json := post.ToJson()
+
+	// write the post to the response as json
+	if _, err := io.WriteString(res, json); err != nil {
+		http.Error(res, err.Error(), http.StatusInternalServerError)
+	} else {
+		res.WriteHeader(http.StatusOK)
+		res.Header().Add("Content-Type", "application/json")
+	}
+}
+
+func renderBadRequestResponse(res http.ResponseWriter, msg string) {
+	fmt.Print(msg)
+	http.Error(res, msg, http.StatusBadRequest)
 }
